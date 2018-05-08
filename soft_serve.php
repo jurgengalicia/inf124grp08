b3ba86391fb6033a4503ec6fa261e2f10a6a0a5c
khuongTiet <khuongtiet@gmail.com>
2018-05-08 11:29:30 -0700

    fixed PHP files

--- soft_serve.php
+++ soft_serve.php
@@ -36,13 +36,13 @@ require_once "pdo.php";
           if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
               echo('<tr><td><a><div class="merchandise">');
-              echo('<img class="zoom" src="'$row['image']'" class="merchandise-image" />')
-              echo('<div class="merchandise-info">'.$row['name'].'</div>')
-              echo('<div class="merchandise-price">'$row['price']'</div>')
+              echo('<img class="zoom" src=".'$row['image'].'" class="merchandise-image" />');
+              echo('<div class="merchandise-info">'.$row['name'].'</div>');
+              echo('<div class="merchandise-price">'.$row['price'].'</div>');
               echo("</div></a></td></tr>\n")
             }
           } else {
-            echo "No matching results found."
+            echo "No matching results found.";
           }
           ?>
         </table>
